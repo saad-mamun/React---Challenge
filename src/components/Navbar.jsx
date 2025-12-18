f86c61f4b963b9cce97d070761233af75e88c7ba
saad-mamun <mamunali88991@gmail.com>
2025-12-18 17:05:41 +0600

    NavlinkAndActiveColor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
+
+  const linkColor = ({isActive}) =>
+    isActive ? "bg-black text-white hover-bg-gray-900 hover:text-white rounded-md px-3 py-2" : "text-black hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md"
+
   return (
-    <div className='bg-blue-300'>
-      <div className='container mx-auto py-3 flex items-center justify-between'>
-        <Link className='text-2xl font-semibold tracking-wide' to="/">React-Challenge</Link>
+    <div className="bg-blue-300">
+      <div className="container mx-auto py-3 flex items-center justify-between">
+        <NavLink className="text-2xl font-semibold tracking-wide" to="/">
+          React-Challenge
+        </NavLink>
         <div>
-            <ul className='flex gap-6'>
-                <Link className='font-semibold text-lg' to="/">Home</Link>
-                <Link className='font-semibold text-lg' to="/jobs">Jobs</Link>
-                <Link className='font-semibold text-lg' to="/add-job">Add Job</Link>
-            </ul>
+          <ul className="flex gap-6">
+            <NavLink className={linkColor} to="/">
+              Home
+            </NavLink>
+            <NavLink className={linkColor} to="/jobs">
+              Jobs
+            </NavLink>
+            <NavLink className={linkColor} to="/add-job">
+              Add Job
+            </NavLink>
+          </ul>
         </div>
       </div>
     </div>
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import { FaExclamationTriangle } from 'react-icons/fa'
 const NotFoundPage = () => {
   return (
     <section className="text-center flex flex-col justify-center items-center  h-96">
-     
+      
         <FaExclamationTriangle className="text-yellow-400 text-6xl mb-4" />
       
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
